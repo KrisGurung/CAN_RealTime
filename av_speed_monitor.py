81fc2e45c1e1b84892aaeb9178b430652558b7e9
Kris Gurung <krisgurung1@gmail.com>
2025-11-24 22:58:41 -0500

    Placeholders for main and listener-daemon added

--- av_speed_monitor.py
+++ av_speed_monitor.py
@@ -2,7 +2,7 @@ import threading
 import time
 import random
 
-# Thread-safe storage of decode CAN values
+# 1) Thread-safe storage of decode CAN values
 class VehicleState:
     def __init__(self):
         self._current_speed = 0.0  # Buffer of Size 1 to store speed value
@@ -18,15 +18,44 @@ class VehicleState:
         # conversion like 'speed = raw * 0.621' (fake conversion factor, just a placeholder!)
         decoded_speed = raw_can_value * 0.621 
         
+        ## CRITICAL SECTION ##
         # MUTEX LOCKING while writing
         with self._lock:
             self._current_speed = decoded_speed
             self._last_updated = time.time()
 
     def get_latest_speed(self):
+        ## CRITICAL SECTION ##
         # MUTEX LOCKING while reading
         with self._lock:
             speed_snapshot = self._current_speed
             timestamp = self._last_updated
         
         return speed_snapshot, timestamp
+
+# 2) Listener (DAEMON)
+def can_bus_callback(vehicle_state):
+
+
+# 3) CONTROL LOOP
+def main_control_loop():
+
+    shared_state = VehicleState()
+
+    # Starting Listener Daemon. Create new thread
+    listener_thread = threading.Thread(
+        target=can_bus_callback, 
+        args=(shared_state,), 
+        daemon=True
+    )
+    listener_thread.start()
+
+    print("[AV CONTROLLER] System initialized. Starting main loop...")
+
+    # MAIN LOOP (Placeholder for CAN detecting logic)
+    try:
+            
+    except:
+
+if __name__ == "__main__":
+    main_control_loop()
